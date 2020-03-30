3e869a3dc0d4016302cd065d147d0c581395c0fd
tbruyn <timtbruyn@gmail.com>
2020-03-30 10:07:36 +0200

    Hyperlink builder

--- final/hyperlink_graph_builder.py
+++ final/hyperlink_graph_builder.py
@@ -10,73 +10,77 @@ import json
 import re
 import pprint
 
+from tqdm import tqdm
+
 
 # Global variables
 G = nx.DiGraph()
 pagedict = {}
-urls_to_visit = []
-urls_visited = []
+
+pages_with_connection_error = {}
+
 
 def init():
     pages = json.loads(open('pages.json','r').read())
+
     [pagedict.update({current_url['url']: current_url}) for current_url in pages]
+
+    [G.add_node(key, attr_data=val) for key, val in pagedict.items()]
 
 
 def find_urls_on_webpage(webpage):
-    website = requests.get(webpage, headers={'Accept-Encoding': 'identity'})
+    try:
+        website = requests.get(webpage, headers={'Accept-Encoding': 'identity'})
+        
+    except Exception as e:
+        print(str(e))
+        pages_with_connection_error[webpage] = str(e)
+        return []
+    
     links = re.findall('"((http|ftp)s?://.*?)"', website.text)
-    return [current_url for current_url, _ in links]
+    return [current_url for current_url, _ in links if current_url in pagedict]
 
+    
 
-def add_url_list_to_graph(page_url, url_list):
-    if not page_url in G and page_url in pagedict:
-        G.add_node(page_url, attr_dict=pagedict[page_url])
-    for url in url_list:
-        if url in pagedict:
-            G.add_node(url, attr_dict=pagedict[url])
-            G.add_edge(page_url, url)
             
 
-def crawl_website(start_url):
-    urls_to_visit.append(start_url)
+def crawl_website():
 
+    iterations = 0
+    this_url = ""
     try:
-        while urls_to_visit:
-            current_url = urls_to_visit.pop()
-            urls_visited.append(current_url)
+        for current_url in tqdm(pagedict):
+            this_url = current_url
 
-            urls_on_webpage = find_urls_on_webpage(current_url)
-            add_url_list_to_graph(current_url, urls_on_webpage)
+            url_list = find_urls_on_webpage(current_url)
 
-            [urls_to_visit.append(url) for url in urls_on_webpage if url in pagedict and not url in urls_visited]
+            [G.add_edge(current_url, new_url) for new_url in url_list]
+            
+            iterations += 1
 
     except Exception as e:
-        error_lists = { 'urls_to_visit' : urls_to_visit,
-                        'urls_visited' : urls_visited       }
+        error_message = str(e) + "\nIterations:" + str(iterations) + "\nCurrent page"+ str(this_url)
         with open('hyperlink_graph_builder_error_log.json', 'w', encoding='utf-8') as file:
-            json.dump(str(e), file, indent=4)
-        with open('hyperlink_graph_builder_error_lists.json', 'w', encoding='utf-8') as file:
-            json.dump(error_lists, file, indent=4)
+            json.dump(error_message, file, indent=4)
+        with open('hyperlink_graph_builder_error_pages.json', 'w', encoding='utf-8') as file:
+            json.dump(pages_with_connection_error, file, indent=4)
         with open('hyperlink_graph_builder_error_output_graph.json', 'w', encoding='utf-8') as file:
             json.dump(json_graph.node_link_data(G), file, indent=4)
         
-
-
-        print("Catch")
+        print("Error occured:\n" + str(e))
 
 init()
 
+crawl_website()
 
-start_url = "https://www.tudelft.nl/"
-
-crawl_website(start_url)
-
-print("urls visited")
-print(urls_visited)
-print("Number of nodes")
-print(len(G.nodes))
-print("Number of edges")
-print(len(G.edges))
+# print("urls visited")
+# print(urls_visited)
+# print("Number of nodes")
+# print(len(G.nodes))
+# print("Number of edges")
+# print(len(G.edges))
 
 with open('hyperlink_graph_builder_output_graph.json', 'w', encoding='utf-8') as file:
     json.dump(json_graph.node_link_data(G), file, indent=4)
+with open('hyperlink_graph_builder_faulty_pages.json', 'w', encoding='utf-8') as file:
+            json.dump(pages_with_connection_error, file, indent=4)
