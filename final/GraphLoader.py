eef46c9da692c764a3c2b9245751e1cc49346bb5
tbruyn <timtbruyn@gmail.com>
2020-04-08 21:02:25 +0200

    Update graphloader

--- final/GraphLoader.py
+++ final/GraphLoader.py
@@ -1,6 +1,13 @@
 import json
 from networkx.readwrite import json_graph
 
+subgraph_filename = '/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/' \
+                    'subgraph_latest.json'
+traffic_graph_filename = '/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/' \
+                         'traffic_graph_latest.json'
+pages_graph_filename = '/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data' \
+                       '/hyperlink_graph_builder_output_graph.json'
+
 
 def load_graph(filename):
     with open(filename) as f:
@@ -8,17 +15,17 @@ def load_graph(filename):
     return json_graph.node_link_graph(js_graph)
 
 
+def load_page_subgraph():
+    return load_graph(subgraph_filename)
+
+
 def load_traffic_graph():
-    filename = '/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/' \
-               'traffic_graph_latest.json'
-    with open(filename) as f:
-        js_graph = json.load(f)
-    return json_graph.node_link_graph(js_graph)
+    return load_graph(traffic_graph_filename)
 
 
 def load_page_graph():
-    filename = '/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data' \
-               '/hyperlink_graph_builder_output_graph.json'
-    with open(filename) as f:
-        js_graph = json.load(f)
-    return json_graph.node_link_graph(js_graph)
+    return load_graph(pages_graph_filename)
+
+
+def filenames():
+    return [subgraph_filename, traffic_graph_filename, pages_graph_filename]
