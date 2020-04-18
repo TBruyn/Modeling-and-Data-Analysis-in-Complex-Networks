5c9052cea1981ba771e0d8c1c6dcc29c873558a7
tbruyn <timtbruyn@gmail.com>
2020-04-18 14:09:16 +0200

    Create two halves of the traffic graph

--- final/crawlers_and_creators/TrafficCombiner.py
+++ final/crawlers_and_creators/TrafficCombiner.py
@@ -3,6 +3,8 @@ import os
 import glob
 import time
 import pprint
+import random
+import math
 
 path = '/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/'
 timestamp = time.asctime(time.localtime(time.time())).replace(' ', '_')
@@ -24,16 +26,27 @@ merged_traffic = {}
 
 traffic_array = list(merged_traffic.values())
 
+choice_1 = random.sample(traffic_array, k=math.floor(len(traffic_array)/2))
+choice_2 = [page for page in traffic_array if page not in choice_1]
+
 with open(path + 'merged_traffic_' + timestamp + '.json', 'w', encoding='utf-8') as file:
     json.dump(merged_traffic, file, indent=4)
 
 with open(path + 'merged_traffic_array_latest.json', 'w', encoding='utf-8') as file:
     json.dump(traffic_array, file, indent=4)
 
+with open(path + 'merged_traffic_array_half_1.json', 'w', encoding='utf-8') as file:
+    json.dump(choice_1, file, indent=4)
+
+with open(path + 'merged_traffic_array_half_2.json', 'w', encoding='utf-8') as file:
+    json.dump(choice_2, file, indent=4)
+
 print("Merged:")
 pprint.pprint(file_list)
 print("Number of visitors in traffic")
 print(len(merged_traffic))
-print("Output file:")
-print(path + 'merged_traffic_' + timestamp + '.json')
+print("Output files:")
 print(path + 'merged_traffic_array_latest.json')
+print(path + 'merged_traffic_' + timestamp + '.json')
+print(path + 'merged_traffic_array_half_1.json')
+print(path + 'merged_traffic_array_half_2.json')
--- final/crawlers_and_creators/TrafficPipeline.py
+++ final/crawlers_and_creators/TrafficPipeline.py
@@ -15,6 +15,18 @@ TrafficCombiner
 print()
 print("Create new traffic graph")
 TrafficGraphMediator.create_graph()
+TrafficGraphMediator.create_graph(
+    outputfile='/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/traffic_graph_half_1.json',
+    inputfile='/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/merged_traffic_array_half_1.json'
+)
+TrafficGraphMediator.create_graph(
+    outputfile='/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/traffic_graph_half_2.json',
+    inputfile='/home/tim/Documents/Modeling-and-Data-Analysis-in-Complex-Networks/final/data/merged_traffic_array_half_2.json'
+)
 print()
 print("Finished traffic pipeline")
+print("Updated:")
+print("\tmerged_traffic_graph_latest.json")
+print("\tmerged_traffic_graph_half_1.json")
+print("\tmerged_traffic_graph_half_2.json")
 print("--------------------------------------------------------------------")
