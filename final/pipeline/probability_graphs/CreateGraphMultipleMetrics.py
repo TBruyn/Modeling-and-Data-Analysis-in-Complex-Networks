d9c50682ab3dfb94f0eb858fb08e6c82c27f91ab
tbruyn <timtbruyn@gmail.com>
2020-04-19 00:09:03 +0200

    pgraph all attributes

--- final/pipeline/probability_graphs/CreateGraphMultipleMetrics.py
+++ final/pipeline/probability_graphs/CreateGraphMultipleMetrics.py
@@ -23,31 +23,47 @@ def add_attribute(graph, attribute_graph, attribute_name):
     [d.update({'p_'+ attribute_name : attribute_graph.edges[(u,v)]['p']}) for u,v,d in graph.edges(data=True)]
     return graph
 
-
-G = GraphLoader.load_page_subgraph()
-
-pageview_graph = CreateGraphPageviewsOnly.create_graph()
-pagerank_graph = CreateGraphPageRankOnly.create_graph()
-betweenness_graph = CreateGraphBetweennessOnly.create_graph()
-# inverted_betweenness_graph = CreateGraphInvertedBetweennessOnly.create_graph()
-degree_graph = CreateGraphDegreeOnly.create_graph()
-inverted_degree_graph = CreateGraphInvertedDegreeOnly.create_graph()
-
-add_attribute(G, pageview_graph, 'page_views')
-add_attribute(G, pagerank_graph, 'pagerank')
-add_attribute(G, betweenness_graph, 'betweenness')
-# add_attribute(G, inverted_betweenness_graph, 'inverted_betweenness')
-add_attribute(G, degree_graph, 'degree')
-add_attribute(G, inverted_degree_graph, 'inv_degree')
-
-GraphLoader.save_probability_graph(G, 'all_attributes', add_timestamp=False)
-
-pprint.pprint(list(G.nodes(data=True))[5])
-pprint.pprint(list(G.edges(data=True))[5])
+def add_probabilities(graph, metric_list_with_weights):
+    for node, data in graph.nodes(data=True):
+        for metric, weight in metric_list_with_weights:
+            data['attr_data']['p_exit'] = data['attr_data']['p_exit_' + metric] * weight
+    for u, v, d in graph.edges(data=True):
+        for metric, weight in metric_list_with_weights:
+            d['p'] = d['p_' + metric] * weight
 
 
-# add_betweenness(G)
-# add_degree(G)
-# add_inverted_degree(G)
-# add_pagerank(G)
+def main():
 
+    G = GraphLoader.load_page_subgraph()
+
+    pageview_graph = CreateGraphPageviewsOnly.create_graph()
+    pagerank_graph = CreateGraphPageRankOnly.create_graph()
+    betweenness_graph = CreateGraphBetweennessOnly.create_graph()
+    # inverted_betweenness_graph = CreateGraphInvertedBetweennessOnly.create_graph()
+    degree_graph = CreateGraphDegreeOnly.create_graph()
+    inverted_degree_graph = CreateGraphInvertedDegreeOnly.create_graph()
+
+    add_attribute(G, pageview_graph, 'page_views')
+    add_attribute(G, pagerank_graph, 'pagerank')
+    add_attribute(G, betweenness_graph, 'betweenness')
+    # add_attribute(G, inverted_betweenness_graph, 'inverted_betweenness')
+    add_attribute(G, degree_graph, 'degree')
+    add_attribute(G, inverted_degree_graph, 'inv_degree')
+
+    metric_list = [
+        ('page_views', 1/5),
+        ('pagerank', 1/5),
+        ('betweenness', 1/5),
+        ('degree', 1/5),
+        ('inv_degree', 1/5)
+    ]
+
+    add_probabilities(G, metric_list)
+
+    GraphLoader.save_probability_graph(G, 'all_attributes', add_timestamp=False)
+    print('Created probability graph with multiple metrics')
+    print('Metrics:')
+    pprint.pprint(metric_list)
+
+if __name__ == "__main__":
+    main()
