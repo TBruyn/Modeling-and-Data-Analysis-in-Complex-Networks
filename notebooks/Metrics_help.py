d91fd46428de4c4f7e7e00debdedf7890174c212
kon91 <kon.chronas@gmail.com>
2020-04-21 13:40:30 +0200

    added jupyter notebook removed files to notebooks folder

--- notebooks/Metrics_help.py
+++ notebooks/Metrics_help.py
@@ -5,25 +5,29 @@ import seaborn as sns
 import collections
 import netcomp as nc
 import numpy as np
-import netlsd
+import netlsd 
+
+
 
 """""
 Use these function to create a dataframe with the networking metrics 
 
 """""
 
-
 def calculate_metrics_df(graph):
+
     df = pd.DataFrame()
     link_density = nx.density(graph)
     info = nx.info(graph)
     av = nx.average_clustering(graph)
 
+
     deg = graph.degree()
     deg_c = nx.degree_centrality(graph)
-    between_c = nx.betweenness_centrality(graph)
+    between_c = nx.betweenness_centrality(graph, normalized=True, endpoints=False)
     pr = nx.pagerank(graph, alpha=0.8)
     close_centrality = nx.closeness_centrality(graph)
+
 
     df['Nodes'] = dict(deg).keys()
     df['Degree'] = dict(deg).values()
@@ -32,12 +36,13 @@ def calculate_metrics_df(graph):
     df['PageRank'] = pr.values()
     df['Closeness Centrality'] = close_centrality.values()
 
+
     print(info)
     print('The density of the graph is :{}'.format(link_density))
-    print('The average clustering coefficient of the graph is {}'.format(av))
-    print('The link density of the graph is {}'.format(nx.density(graph)))
+    print('The average clustering coefficient of the graph'.format(av))
 
     return df
+
 
 
 """""
@@ -46,12 +51,11 @@ Use this function to visualize the graph where the nodes size is
 based on the node betweeness value.
 
 """""
-
-
 def plot_graph(graph):
+
     pos = nx.spring_layout(graph)
     betCent = nx.betweenness_centrality(graph, normalized=True, endpoints=True)
-    node_color = [20000.0 * graph.degree(v) for v in graph]
+    node_color = [20000.0 * graph.degree(v) for v in graph ]
     node_size = [v * 10000 for v in betCent.values()]
     plt.figure(figsize=(20, 20))
     nx.draw_networkx(graph, pos=pos, with_labels=False,
@@ -61,6 +65,7 @@ def plot_graph(graph):
 
 
 def plot_distributions(data):
+
     plt.figure(figsize=(10, 5))
     sns.kdeplot(data["Closeness Centrality"], color="skyblue")
     sns.kdeplot(data["Degree Centrality"], color="olive")
@@ -96,53 +101,38 @@ def plot_degree_distribution(dd):
     plt.xlabel("Degree")
     plt.xlim(0, 75)
     plt.show()
-
-
+    
 def Comparison_metrics(G1, G2):
+    
     A1 = nx.adjacency_matrix(G1)
     A2 = nx.adjacency_matrix(G2)
-
-    a = nc.lambda_dist(A1, A2, kind='adjacency')
-    l = nc.lambda_dist(A1, A2, kind='laplacian')
+    
+    a = nc.deltacon0(A1,A2)
+    b = nc.lambda_dist(A1,A2)
+    c = nc.lambda_dist(A1,A2,kind='laplacian_norm')
+    
+    av_cl1 = nx.average_clustering(G1)
+    av_cl2 = nx.average_clustering(G1)
+    
     desc0 = netlsd.heat(G1)
     desc1 = netlsd.heat(G2)
     net__lsd = netlsd.compare(desc0, desc1)
+    
+    print('The Average Glustering coeficient of G1:{}'.format(av_cl1))
+    print('The Average Glustering coeficient of G2:{}'.format(av_cl2))
+    
+    print('The DeltaCon of G1 and G2:{}'.format(a))
+    print('The differnce of the adjcency matrices of G1 and G2'.format(b))
+    print('The differnce of the laplacian matrices of G1 and G2:{}'.format(c))
+    print('The Netlsd of G1 and G2:{}'.format(net__lsd))
 
-    return a, l, net__lsd
-
-
-def comparison_df(traffic_graph, graphs):
-
-    A_d = []
-    L_d = []
-    Net = []
-    for i, c in enumerate(graphs.values()):
-        x, y, z = Comparison_metrics(traffic_graph, c)
-        A_d.append(x)
-        L_d.append(y)
-        Net.append(z)
-
-    comp = pd.DataFrame()
-    comp['Graph'] = graphs.keys()
-    comp['A Euclidean Dis'] = A_d
-    comp['L Euclidean Dis'] = L_d
-    comp['NetLsd'] = Net
-
-    return comp
+    
+def plot_comp(d1,d2):
+    f, axes = plt.subplots(1, 2, figsize=(10, 5))
+    sns.kdeplot(d1['Degree Centrality'],color='b', ax=axes[0, 0])
+    sns.kdeplot(d2['Degree Centrality'], color= 'r', ax=axes[0, 0])
 
 
-
-
-def plot_comp(d1, d2):
-    plt.figure(figsize=(10, 5))
-    sns.kdeplot(d1['Degree'], color='b', label= 'Traffic Graph')
-    sns.kdeplot(d2['Degree'], color='r', label= 'P Graph')
-    plt.title('Degree distribution of Traffic graph vs P Graph')
+    sns.kdeplot(d1["Betweeness Centrality"],color="b", ax=axes[0, 1])
+    sns.kdeplot(d2["Betweeness Centrality"], color="r", ax=axes[0, 1]')
     plt.show()
-
-
-def netlsd_func(G1, G2):
-    desc0 = netlsd.heat(G1)
-    desc1 = netlsd.heat(G2)
-    distance = netlsd.compare(desc0, desc1)
-    return distance
